fb560f155bb00a5c842fbf57fcae336b6fafc57e
Wilton Guilherme <wiltoninfra@gmail.com>
2024-03-10 00:29:05 -0300

    Update dockerfile frontend

--- release.config.js
+++ release.config.js
@@ -3,7 +3,9 @@ module.exports = {
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
-      '@semantic-release/github',
-      '@semantic-release/git'
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/git',
+      '@semantic-release/exec'
     ]
   };
